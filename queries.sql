ac4fa29cc1c39525bdd4be7ae71508669a4d0b3b
chandannayak-telus <116580846+chandannayak-telus@users.noreply.github.com>
2023-11-09 14:58:03 -0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,59 +1,106 @@
 -- revenue from Apr 2023 campaign
+--msidn instead of ban
 
-with bans as
-(select distinct(BAN) from `wb-ai-acltr-tbs-1-pr-cb6990.smb_roaming_campaign.apr2023final`)
-select sum(B.REV_MRC_F_ROAM_Easy)  from bans A
-left join `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue` B
-ON A.BAN = B.BAN
-where B.CCYYMM = 202304
-AND SEGMENT_ROLLUP_2_CUR LIKE '%SMB%'
+
+with apr_rev as (
+select BAN, MOBILE_PHONE_NUMBER as MSISDN, REV_MRC_F_ROAM_Easy as rev from `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue`
+where CCYYMM = 202304
+AND SEGMENT_ROLLUP_2_CUR LIKE '%SMB%')
+select count(distinct(apr_rev.MSISDN)) as total_users,
+      sum(apr_rev.rev) as roaming_revenue
+from apr_rev
+where apr_rev.BAN in (select distinct(BAN) from `wb-ai-acltr-tbs-1-pr-cb6990.smb_roaming_campaign.apr2023final`)
 
 -- revenue from June 2023 campaign
 
-with bans as
-(select distinct(BAN) from `wb-ai-acltr-tbs-1-pr-cb6990.smb_roaming_campaign.jun2023final`)
-select sum(B.REV_MRC_F_ROAM_Easy)  from bans A
-left join `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue` B
-ON A.BAN = B.BAN
-where B.CCYYMM = 202306
-AND SEGMENT_ROLLUP_2_CUR LIKE '%SMB%'
+
+with june_rev as (
+select BAN, MOBILE_PHONE_NUMBER as MSISDN, REV_MRC_F_ROAM_Easy as rev from `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue`
+where CCYYMM = 202306
+AND SEGMENT_ROLLUP_2_CUR LIKE '%SMB%')
+select count(distinct(june_rev.MSISDN)) as total_users,
+      sum(june_rev.rev) as roaming_revenue
+from june_rev
+where june_rev.BAN in (select distinct(BAN) from `wb-ai-acltr-tbs-1-pr-cb6990.smb_roaming_campaign.jun2023final`)
 
 -- revenue from July 2023 campaign
 
-with bans as
-(select distinct(BAN) from `wb-ai-acltr-tbs-1-pr-cb6990.smb_roaming_campaign.jul2023final`)
-select sum(B.REV_MRC_F_ROAM_Easy)  from bans A
-left join `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue` B
-ON A.BAN = B.BAN
-where B.CCYYMM = 202307
-AND SEGMENT_ROLLUP_2_CUR LIKE '%SMB%'
+
+with june_rev as (
+select BAN, MOBILE_PHONE_NUMBER as MSISDN, REV_MRC_F_ROAM_Easy as rev from `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue`
+where CCYYMM = 202306
+AND SEGMENT_ROLLUP_2_CUR LIKE '%SMB%')
+select count(distinct(june_rev.MSISDN)) as total_users,
+      sum(june_rev.rev) as roaming_revenue
+from june_rev
+where june_rev.BAN in (select distinct(BAN) from `wb-ai-acltr-tbs-1-pr-cb6990.smb_roaming_campaign.jun2023final`)
 
 -- revenue from Aug 2023 campaign
 
-with bans as
-(select distinct(BAN) from `wb-ai-acltr-tbs-1-pr-cb6990.smb_roaming_campaign.aug2023final`)
-select sum(B.REV_MRC_F_ROAM_Easy)  from bans A
-left join `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue` B
-ON A.BAN = B.BAN
-where B.CCYYMM = 202308
-AND SEGMENT_ROLLUP_2_CUR LIKE '%SMB%'
+with aug_rev as (
+select BAN, MOBILE_PHONE_NUMBER as MSISDN, REV_MRC_F_ROAM_Easy as rev from `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue`
+where CCYYMM = 202308
+AND SEGMENT_ROLLUP_2_CUR LIKE '%SMB%')
+select count(distinct(aug_rev.MSISDN)) as total_users,
+      sum(aug_rev.rev) as roaming_revenue
+from aug_rev
+where aug_rev.BAN in (select distinct(BAN) from `wb-ai-acltr-tbs-1-pr-cb6990.smb_roaming_campaign.aug2023final`)
 
 -- revenue from Sep 2023 campaign
 
-with bans as
-(select distinct(BAN) from `wb-ai-acltr-tbs-1-pr-cb6990.smb_roaming_campaign.sep2023final`)
-select sum(B.REV_MRC_F_ROAM_Easy)  from bans A
-left join `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue` B
-ON A.BAN = B.BAN
-where B.CCYYMM = 202309
-AND SEGMENT_ROLLUP_2_CUR LIKE '%SMB%'
+with sep_rev as (
+select BAN, MOBILE_PHONE_NUMBER as MSISDN, REV_MRC_F_ROAM_Easy as rev from `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue`
+where CCYYMM = 202309
+AND SEGMENT_ROLLUP_2_CUR LIKE '%SMB%')
+select count(distinct(sep_rev.MSISDN)) as total_users,
+      sum(sep_rev.rev) as roaming_revenue
+from sep_rev
+where sep_rev.BAN in (select distinct(BAN) from `wb-ai-acltr-tbs-1-pr-cb6990.smb_roaming_campaign.sep2023final`)
 
-with jun_50pile as (
-select ban from `bi-srv-aaaie-pr-c0c268.roaming_pr.bq_smb_traveler_roaming`
-where dl_vol_percentile >= 30
-and aggregated_from = '2023-06-01')
-select sum(b.REV_MRC_F_ROAM_Easy) from jun_50pile as a
-left join `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue` as b
-on a.ban = b.BAN
+
+
+
+
+
+
+-- HIGH PROPENSITY SCORES
+
+-- April 2023
+select count(distinct(MOBILE_PHONE_NUMBER)) as high_prop_users,sum(REV_MRC_F_ROAM_Easy) as high_prop_revenue from `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue` b
+where b.CCYYMM = 202304
+AND b.SEGMENT_ROLLUP_2_CUR LIKE '%SMB%'
+and b.ban in (select distinct(ban) from `bi-srv-aaaie-pr-c0c268.roaming_pr.bq_smb_traveler_roaming`
+where aggregated_from = '2023-06-01'
+and dl_vol_percentile >= 50)
+
+-- June 2023
+select count(distinct(MOBILE_PHONE_NUMBER)) as high_prop_users,sum(REV_MRC_F_ROAM_Easy) as high_prop_revenue from `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue` b
+where b.CCYYMM = 202306
+AND b.SEGMENT_ROLLUP_2_CUR LIKE '%SMB%'
+and b.ban in (select distinct(ban) from `bi-srv-aaaie-pr-c0c268.roaming_pr.bq_smb_traveler_roaming`
+where aggregated_from = '2023-06-01'
+and dl_vol_percentile >= 50)
+
+-- July 2023
+select count(distinct(MOBILE_PHONE_NUMBER)) as high_prop_users,sum(REV_MRC_F_ROAM_Easy) as high_prop_revenue from `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue` b
 where b.CCYYMM = 202307
 AND b.SEGMENT_ROLLUP_2_CUR LIKE '%SMB%'
+and b.ban in (select distinct(ban) from `bi-srv-aaaie-pr-c0c268.roaming_pr.bq_smb_traveler_roaming`
+where aggregated_from = '2023-07-01'
+and dl_vol_percentile >= 50)
+
+-- August 2023
+select count(distinct(MOBILE_PHONE_NUMBER)) as high_prop_users,sum(REV_MRC_F_ROAM_Easy) as high_prop_revenue from `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue` b
+where b.CCYYMM = 202308
+AND b.SEGMENT_ROLLUP_2_CUR LIKE '%SMB%'
+and b.ban in (select distinct(ban) from `bi-srv-aaaie-pr-c0c268.roaming_pr.bq_smb_traveler_roaming`
+where aggregated_from = '2023-08-01'
+and dl_vol_percentile >= 50)
+
+-- Sept 2023
+select count(distinct(MOBILE_PHONE_NUMBER)) as high_prop_users,sum(REV_MRC_F_ROAM_Easy) as high_prop_revenue from `wb-ai-acltr-tbs-1-pr-cb6990.ref_table.pricing_wls_network_revenue` b
+where b.CCYYMM = 202309
+AND b.SEGMENT_ROLLUP_2_CUR LIKE '%SMB%'
+and b.ban in (select distinct(ban) from `bi-srv-aaaie-pr-c0c268.roaming_pr.bq_smb_traveler_roaming`
+where aggregated_from = '2023-09-01'
+and dl_vol_percentile >= 50)
